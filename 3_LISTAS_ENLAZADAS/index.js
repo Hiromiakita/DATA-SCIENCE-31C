3fe8e24fc2c55bfde5e85c5dd4bb8543ef71a2d9
Sensei Stiven <strujillodev@gmail.com>
2024-02-01 21:01:32 -0500

    feat: first class linked list

---
+++ 3_LISTAS_ENLAZADAS/index.js
@@ -0,0 +1,17 @@
+class Node {
+
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+
+  // constructor() {
+  //   this.head = null;
+  // }
+  constructor(head = null) {
+    this.head = head;
+  }
+}
