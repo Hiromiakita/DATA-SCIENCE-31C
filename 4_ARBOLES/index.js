a3aae466c656d0ea7dca5ba5368d1b43c0829057
Sensei Stiven <strujillodev@gmail.com>
2024-02-15 22:31:37 -0500

    feat: ejemplo practico arboles autocompletar

--- 4_ARBOLES/index.js
+++ 4_ARBOLES/index.js
@@ -80,3 +80,107 @@ tree2.insert(1)
 tree2.insert(10)
 
 console.log(areIndenticalTrees(tree1.root, tree2.root));
+
+
+// 2.- Escribe una función que dado un árbol binario A, obtenga una copia B del mismo.
+
+function copyBinaryTree(root) {
+    if (!root) return null;
+
+    function copyNodes(node) {
+        if (!node) return null;
+
+        const newNode = new TreeNode(node.value); // copia del nodo
+
+        newNode.left = copyNodes(node.left);
+        newNode.right = copyNodes(node.right);
+
+        return newNode;
+    }
+
+    const newRoot = copyNodes(root); // node root
+    const newTree = new BinaryTree();
+    newTree.root = newRoot;
+
+    return newTree;
+}
+
+// Ejemplo de uso:
+const copiedTree = copyBinaryTree(tree2.root)
+
+console.log(copiedTree);
+
+
+
+
+/// ----------------------------------
+// EJEMPLO PRACTICO (AUTOCOMPLETAR)
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class AutoComplete {
+    constructor() {
+        this.root = null;
+    }
+
+    addWord(word) {
+        const node = new Node(word);
+        this.addWordAux(node, this.root);
+    }
+
+    addWordAux(node, current) {
+        if (current === null) {
+            this.root = node;
+            return
+        }
+
+        if (node.value < current.value) {
+            if (current.left === null) {
+                current.left = node;
+            } else {
+                this.addWordAux(node, current.left);
+            }
+        } else {
+            if (current.right === null) {
+                current.right = node;
+            } else {
+                this.addWordAux(node, current.right);
+            }
+        }
+    }
+
+    getSuggestions(prefix) {
+        const suggestions = [];
+        this.getSuggestionsAux(prefix, this.root, suggestions)
+        return suggestions;
+    }
+
+    getSuggestionsAux(prefix, node, suggestions) {
+        if (node === null) {
+            return;
+        }
+
+        if (node.value.startsWith(prefix)) {
+            suggestions.push(node.value);
+        }
+
+        this.getSuggestionsAux(prefix, node.left, suggestions);
+        this.getSuggestionsAux(prefix, node.right, suggestions);
+    }
+}
+
+const autocompletar = new AutoComplete();
+autocompletar.addWord("abandonar");
+autocompletar.addWord("abajo");
+autocompletar.addWord("acabar");
+autocompletar.addWord("accceder");
+autocompletar.addWord("bella");
+autocompletar.addWord("bola");
+
+const suggestions = autocompletar.getSuggestions("be");
+console.log(suggestions);
